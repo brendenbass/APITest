598dce6cf0e4be54353ef482c9b0b4705dacb1e8
Brenden <ibrenden@icloud.com>
2023-03-28 13:50:05 -0400

    I FORGOT TO PUT AWAIT

--- functions/fetch-events/fetch-events.js
+++ functions/fetch-events/fetch-events.js
@@ -1,9 +1,9 @@
 // const handler = async (event) => {
 //   const API_USER_ID = "b55d31245eed38be8b289fce8a41b7c82d0bd17229f0d3e14d89cddfead62dd8"
 //   const API_SECRET = "3fb446ebf099196055056feaa74af8f4cd9c26068c1bb97ef917d9210d59cf8c"
+const axios = require('axios');
 
 const handler = async () => {
-  const axios = require('axios');
   
   const username = process.env.API_USER_ID
   const password = process.env.API_SECRET
@@ -11,7 +11,7 @@ const handler = async () => {
   const url = 'https://api.planningcenteronline.com/calendar/v2/event_instances?where[event_name]=Ocala.Youth&include=event&filter=future,approved&order=starts_at&per_page=5'
 
   try {
-    const { data } = axios.get(url, { auth: { username, password } })
+    const { data } = await axios.get(url, { auth: { username, password } })
 
     return {
       statusCode: 200,
