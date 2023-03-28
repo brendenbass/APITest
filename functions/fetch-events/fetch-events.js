c646d4ca09a3defcfa70b969a156a084eecd3c4f
Brenden <ibrenden@icloud.com>
2023-03-28 12:36:51 -0400

    update

--- functions/fetch-events/fetch-events.js
+++ functions/fetch-events/fetch-events.js
@@ -1,12 +1,12 @@
 const handler = async () => {
   
-  const API_USER_ID = "b55d31245eed38be8b289fce8a41b7c82d0bd17229f0d3e14d89cddfead62dd8"
-  const API_SECRET = "3fb446ebf099196055056feaa74af8f4cd9c26068c1bb97ef917d9210d59cf8c"
-  var headers = new Headers({'Authorization': `Basic ${btoa(API_USER_ID + ':' + API_SECRET)}`})
+  const API_USER_ID = process.env.API_USER_ID
+  const API_SECRET = process.env.API_SECRET
+  var auth = 'Basic ' + Buffer.from(API_USER_ID + ':' + API_SECRET).toString('base64');
   const url = "https://api.planningcenteronline.com/calendar/v2/event_instances?per_page=4&order=starts_at&where[tag_ids]=214556&include=event&filter=future,approved";
 
   try {
-    const response = await fetch(url, { headers: headers })
+    const response = await fetch(url, { headers: auth })
 
     var data = await response.json();
 
